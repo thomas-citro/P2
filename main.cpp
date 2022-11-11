8c7ed017f91ce09960ae4f19131f40b65b296573
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-11-10 23:40:24 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <ctype.h>
+#include "prototypes.h"
+using namespace std;
+
+
+/* Global variables */
+char otherSymbols[16] = {'+', '-', '*', '/', '^', '.', '(', ')', ',', '{',
+                         '}', ';', '[', ']', '<', '>'};
+int FSA[9][12] = {
+    {1, 2, 0, 1004, 1, 3, 4, 5, 6, 7, 8, -1},
+    {1, 1, 1001, 1001, 1001, 1001, 1001, 1001, 1001, 1001, 1001, 1001},
+    {1002, 2, 1002, 1002, 1002, 1002, 1002, 1002, 1002, 1002, 1002, 1002},
+    {1003, 1003, 1003, 1003, 1003, 8, 1003, 1003, 1003, 1003, 1003, 1003},
+    {1003, 1003, 1003, 1003, 1003, 8, 1003, 1003, 1003, 1003, 1003, 1003},
+    {1003, 1003, 1003, 1003, 1003, 8, 1003, 1003, 1003, 1003, 1003, 1003},
+    {1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 8, 1003, 1003, 1003},
+    {1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 8, 1003, 1003},
+    {1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003, 1003}
+};
+string keywords[17] = {"begin", "end", "do", "while", "whole", "label",
+                       "return", "input", "output", "program", "warp",
+                       "if", "then", "pick", "declare", "assign", "func"};
+int tokenID;
+struct Node* tree;
+
+
+int main() {
+    /* Declare/initialize variables */
+    vector<vector<string>> tokens;
+    fstream newfile;
+    bool processingComment = false;
+    int state = 0;
+    int lineNumber = 1;
+    int lineLength = 0;
+    
+    /* Process file line by line (Scanner) */
+    newfile.open("testFile.txt",ios::in);
+    if (newfile.is_open()) {
+        string currentLine;
+        while(getline(newfile, currentLine)) {
+            lineLength = currentLine.length();
+            processLine(currentLine, tokens, processingComment, state, lineNumber);
+            lineNumber++;
+        }
+        newfile.close();
+    }
+    vector<string> innerVector{"eofTK", "", to_string(lineNumber - 1), to_string(lineLength)};
+    tokens.push_back(innerVector);
+    outputVector(tokens);
+    
+    /* Start the parser */
+    tokenID = 0;
+    parser(tokens);
+}
+
+
+void processLine(string line, vector<vector<string>>& tokens, bool& processingComment, int& state, int lineNumber) {
+    string currentToken = "";
+    string tokenType = "";
+    int tokenFirstChar = 0;
+    int i;
+    for (i = 0; i < line.length(); i++) {
+        if (line[i] == '#' && processingComment) {
+            processingComment = false;
+            continue;
+        } else if (line[i] == '#' && !processingComment) {
+            processingComment = true;
+            continue;
+        }
+        if (processingComment) {
+            continue;
+        }
+        processCharacter(currentToken, tokenType, tokenFirstChar, tokens, processingComment, state, lineNumber, line[i], i);
+    }
+    /* Process end of line */
+    processCharacter(currentToken, tokenType, tokenFirstChar, tokens, processingComment, state, lineNumber, ' ', i);
+}
+
+
+void processCharacter(string& currentToken, string& tokenType, int& tokenFirstChar, vector<vector<string>>& tokens, bool& processingComment, int& state, int lineNumber, char currentChar, int& i) {
+    int FSA_Value = FSA[state][getColumn(currentChar)];
+    if (FSA_Value > 1000) {
+        if (FSA_Value == 1001) {
+            tokenType = "identifierToken";
+            bool keyword = false;
+            int arraySize = sizeof(keywords) / sizeof(string);
+            for (int j = 0; j < arraySize; j++) {
+                if (keywords[j] == currentToken) {
+                    keyword = true;
+                }
+            }
+            if (keyword) {
+                tokenType = "keywordToken";
+            }
+        } else if (FSA_Value == 1002) {
+            tokenType = "integerToken";
+        } else if (FSA_Value == 1003) {
+            tokenType = "operatorToken";
+        }
+        vector<string> innerVector{tokenType, currentToken, to_string(lineNumber), to_string(tokenFirstChar)};
+        tokens.push_back(innerVector);
+        currentToken = "";
+        state = 0;
+        i--;
+    } else {
+        if (!isspace(currentChar)) {
+            if (currentToken.empty()) {
+                tokenFirstChar = i;
+            }
+            currentToken += currentChar;
+        }
+        state = FSA_Value;
+    }
+}
+
+
+int getColumn(char c) {
+    int column;
+    if (isalpha(c)) {
+        column = 0;
+    } else if (isdigit(c)) {
+        column = 1;
+    } else if (isspace(c)) {
+        column = 2;
+    } else if (c == '_') {
+        column = 4;
+    } else if (c == '=') {
+        column = 5;
+    } else if (c == '!') {
+        column = 6;
+    } else if (c == ':') {
+        column = 7;
+    } else if (c == '|') {
+        column = 8;
+    } else if (c == '&') {
+        column = 9;
+    } else {
+        bool charFound = false;
+        int arraySize = sizeof(otherSymbols) / sizeof(char);
+        for (int i = 0; i < arraySize; i++) {
+            if (otherSymbols[i] == c) {
+                charFound = true;
+            }
+        }
+        if (charFound) {
+            column = 10;
+        } else {
+            cout << "Unrecognized symbol: " << c << endl;
+            exit(0);
+        }
+    }
+    return column;
+}
+
+void outputVector(vector<vector<string>>& myVector) {
+    cout << "--- Vector ---" << endl;
+    for (int i = 0; i < myVector.size(); i++) {\
+        cout << "[" << i << "] = ['" << myVector[i][0] << "', '" << myVector[i][1] << "', '" << myVector[i][2] 
+        << "', '" << myVector[i][3] << "']" << endl;
+    }
+    cout << "--------------" << endl;
+}
+
