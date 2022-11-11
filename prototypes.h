541cf1b5d007995e34edafbf03cdd20d7d3b2983
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-11-10 23:41:46 -0600

    Create prototypes.h

---
+++ prototypes.h
@@ -0,0 +1,26 @@
+// Structs
+struct Node {
+	string token;
+	struct Node *first, *second, *third, *fourth;		
+};
+
+
+// Function prototypes for main/scanner
+void outputVector(vector<vector<string>>&);
+void processLine(string, vector<vector<string>>&, bool&, int&, int);
+void processCharacter(string&, string&, int&, vector<vector<string>>&, bool&, int&, int, char, int&);
+int getColumn(char);
+
+
+// Function prototypes for parser
+void parser(vector<vector<string>>&);
+void vars(vector<vector<string>>&);
+void block(vector<vector<string>>&);
+void parserError(string, int);
+
+
+// Function prototypes for tree
+struct Node* createTree(string);
+struct Node* insertNode(struct Node*, string);
+void addSubtree(struct Node*, struct Node*);
+void inorderTraversal(struct Node*, int);
