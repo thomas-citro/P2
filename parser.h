cb5387b91e09bcdcd6d0d34301aac0e0d9122f0c
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-11-13 20:35:23 -0600

    Added more function prototypes for parser

--- parser.h
+++ parser.h
@@ -22,11 +22,28 @@ int getColumn(char);
 
 
 // Function prototypes for parser
-void parser(std::vector<std::vector<std::string>>&);
-node* vars(std::vector<std::vector<std::string>>&);
-node* block(std::vector<std::vector<std::string>>&);
 void parserError(std::string, int);
 token* nonterminal(std::string);
 token* terminal(std::vector<std::string>);
+void parser(std::vector<std::vector<std::string>>&);
+node* vars(std::vector<std::vector<std::string>>&);
+node* block(std::vector<std::vector<std::string>>&);
+node* expr(std::vector<std::vector<std::string>>&);
+node* N(std::vector<std::vector<std::string>>&);
+node* A(std::vector<std::vector<std::string>>&);
+node* A2(std::vector<std::vector<std::string>>&);
+node* M(std::vector<std::vector<std::string>>&);
+node* R(std::vector<std::vector<std::string>>&);
+node* stats(std::vector<std::vector<std::string>>&);
+node* mStat(std::vector<std::vector<std::string>>&);
+node* stat(std::vector<std::vector<std::string>>&);
+node* in(std::vector<std::vector<std::string>>&);
+node* out(std::vector<std::vector<std::string>>&);
+node* _if(std::vector<std::vector<std::string>>&);
+node* loop(std::vector<std::vector<std::string>>&);
+node* assign(std::vector<std::vector<std::string>>&);
+node* RO(std::vector<std::vector<std::string>>&);
+node* label(std::vector<std::vector<std::string>>&);
+node* _goto(std::vector<std::vector<std::string>>&);
 
 #endif
